994bc42a7163e6d87a04e4781cd2a8393e1d33fd
Susmoy-Das02 <methundas269@gmail.com>
2022-05-22 14:47:07 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Navbar from './Pages/Shared/Navbar';
+
+function App() {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Routes>
+
+      </Routes>
+      
+    </div>
+  );
+}
+
+export default App;
