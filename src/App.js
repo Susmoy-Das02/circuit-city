5d004bbca516e084b26b085a7e98328f3c7f2e50
Susmoy-Das02 <methundas269@gmail.com>
2022-05-24 01:55:41 +0600

    require auth done

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Blogs from './Pages/Home/Blogs';
 import Home from './Pages/Home/Home';
+import ItemDetails from './Pages/Home/ItemDetails';
 import Login from './Pages/Login/Login';
+import RequireAuth from './Pages/Login/RequireAuth';
 import SignUp from './Pages/Login/SignUp';
 import Navbar from './Pages/Shared/Navbar';
 
@@ -16,6 +18,13 @@ function App() {
         <Route path='blogs' element={<Blogs></Blogs>}></Route>
         <Route path='login' element={<Login></Login>}></Route>
         <Route path='signup' element={<SignUp></SignUp>}></Route>
+        <Route path='/item/:itemId' element={
+
+          <RequireAuth>
+            <ItemDetails></ItemDetails>
+          </RequireAuth>
+
+        }></Route>
 
       </Routes>
       
--- src/Pages/Home/Item.js
+++ src/Pages/Home/Item.js
@@ -1,20 +1,26 @@
 import React from 'react';
+import {useNavigate} from 'react-router-dom';
 
 const Item = ({ item }) => {
-    const {name, img, perUnitPrice, minimumOrderQuantity, description, availableQuantity} = item;
+    const {_id, name, img, perUnitPrice, minimumOrderQuantity, description, availableQuantity} = item;
+    const navigate = useNavigate();
+
+    const navigateToItemDetail = id =>{
+        navigate(`/item/${id}`);
+    }
     return (
         <div class="card lg:max-w-lg bg-base-100 shadow-xl">
             <figure class="px-10 pt-10">
                 <img src={img} alt="Shoes" class="rounded-xl" />
             </figure>
             <div class="card-body items-center text-center">
-                <h2 class="card-title">{name}</h2>
-                <h3>Per unit price :{perUnitPrice}</h3>
+                <h2 class="card-title text- secondary">{name}</h2>
+                <h3 className='text-2xl'>Per unit price :{perUnitPrice}</h3>
                 <h3>Minimum order quantity :{minimumOrderQuantity}</h3>
                 <h3>Available quantity :{availableQuantity}</h3>
                 <p>{description}</p>
                 <div class="card-actions">
-                    <button class="btn btn-primary">Buy Now</button>
+                    <button  onClick={() => navigateToItemDetail(_id)} class="btn btn-primary">purchase</button>
                 </div>
             </div>
         </div>
