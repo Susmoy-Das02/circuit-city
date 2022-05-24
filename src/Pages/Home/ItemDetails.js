ed813bca9b47ebfaa503e25c91ebb5224b32cc92
Susmoy-Das02 <methundas269@gmail.com>
2022-05-24 15:59:51 +0600

    data load from server

--- src/Pages/Home/ItemDetails.js
+++ src/Pages/Home/ItemDetails.js
@@ -1,11 +1,20 @@
-import React from 'react';
-import {useParams} from 'react-router-dom';
+import React, { useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 const ItemDetails = () => {
-    const{itemId} = useParams();
+    const { itemId } = useParams();
+
+
+
     return (
-        <div>
-            
+        <div class="card w-96 bg-base-100 shadow-xl">
+            <div class="card-body">
+                <h2 class="card-title">Card title!</h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <div class="card-actions justify-end">
+                    <button class="btn btn-primary">Buy Now</button>
+                </div>
+            </div>
         </div>
     );
 };
